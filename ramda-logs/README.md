502738852a31a6470733bbd7c64c294b927b8aeb
Kevin Beaty <kevin@simplectic.com>
2015-03-22 19:25:45 -0500

    Initial example code for parsing log files with ramda post

---
+++ ramda-logs/README.md
@@ -0,0 +1,1 @@
+Example code for [Streaming Logs with Transducers and Ramda](http://simplectic.com/blog/2015/ramda-transducers-logs/)
---
+++ ramda-logs/pagevisits.js
@@ -0,0 +1,45 @@
+'use strict'
+var R = require('ramda')
+var stream = require('transduce-stream')
+var lines = require('transduce/string/lines')
+
+// Is line a GET request for something other than static?
+var isGet = R.test(/GET \//)
+var notStatic = R.complement(R.test(/GET \/static/))
+var isPage = R.allPass([isGet, notStatic])
+
+// 'log line' -> ['IP', 'GET /url/path']
+var splitLine = R.pipe(
+      R.match(/^(\S+).+"([^"]+)"/),
+      R.tail)
+
+// 'GET /url/path' -> 'http://simplectic.com/url/path'
+var toURL = R.pipe(
+      R.split(' '),
+      R.slice(1, 2),
+      R.prepend('http://simplectic.com'),
+      R.join(''))
+
+// Lens to operate on value in [key, value] pair
+var valueLens = R.lens(
+      R.last,
+      R.flip(R.converge(R.append, R.nthArg(1), R.init)))
+
+// ['IP', 'GET /url/path'] -> ['IP', 'http://simplectic.com/url/path']
+var valueToUrl = valueLens.map(toURL)
+
+var parseLog = R.compose(
+      // step lines as it is streamed
+      lines(),
+      // filter non-static GET requests
+      R.filter(isPage),
+      // -> ['IP', 'GET /url/path']
+      R.map(splitLine),
+      // -> ['IP', 'http://simplectic.com/url/path']
+      R.map(valueToUrl),
+      // -> 'IP visited http://simplectic.com/url/path\n'
+      R.map(R.join(' visited ')),
+      R.map(R.add(R.__, '\n')))
+    
+process.stdin.pipe(stream(parseLog)).pipe(process.stdout)
+process.stdin.resume()
